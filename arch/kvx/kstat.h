ae5955d07ae3d132049a4418a8b77affa6d82b44
Jonathan Borne <jborne@kalray.eu>
2021-10-01 14:41:59 +0200

    kvx: add missing kstat.h header
    Summary: Building linux with musl support fails with
    
    src/stat/fchmodat.c:5:10: fatal error:
    kstat.h: No such file or directory
    
    This revision provide the missing kstat.h header.
    
    Test Plan: Buildroot + musl
    
    Reviewers: O52 Linux libcs Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T12378
    
    Differential Revision: https://phab.kalray.eu/D15454

---
+++ arch/kvx/kstat.h
@@ -0,0 +1,22 @@
+struct kstat {
+	dev_t st_dev;
+	ino_t st_ino;
+	mode_t st_mode;
+	nlink_t st_nlink;
+	uid_t st_uid;
+	gid_t st_gid;
+	dev_t st_rdev;
+	unsigned int    __pad0;
+	off_t st_size;
+	blksize_t st_blksize;
+	unsigned int    __pad1;
+	blkcnt_t st_blocks;
+
+	long st_atime_sec;
+	long st_atime_nsec;
+	long st_mtime_sec;
+	long st_mtime_nsec;
+	long st_ctime_sec;
+	long st_ctime_nsec;
+	long __unused[2];
+};
